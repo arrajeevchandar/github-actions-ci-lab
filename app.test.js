5a1987198272e134d1da8a45535a486a268b5f6d
arrajeevchandar <rajeevarchandar@gmail.com>
2026-09-22 10:26:07 +0530

    Add multiplication function and test

--- app.test.js
+++ app.test.js
@@ -1,4 +1,4 @@
-const { add, subtract } = require('./app');
+const { add, subtract, multiply } = require('./app');
 
 test('adds 2 + 3 to equal 5', () => {
     expect(add(2, 3)).toBe(5);
@@ -6,4 +6,8 @@ test('adds 2 + 3 to equal 5', () => {
 
 test('subtracts 3 from 10 to equal 7', () => {
     expect(subtract(10, 3)).toBe(7);
+});
+
+test('multiplies 4 by 5 to equal 20', () => {
+    expect(multiply(4, 5)).toBe(20);
 });
